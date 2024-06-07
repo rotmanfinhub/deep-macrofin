e8909972fa2b7904606fc2dbe21f09bf741a2a2b
Flora Guo <guoflora5@gmail.com>
2024-06-07 19:22:59 -0400

    Implemented equation.py, hjb_equation.py and constraint.py, tested them in test_equation.py

--- deepMacroFin/evaluations/constraint.py
+++ deepMacroFin/evaluations/constraint.py
@@ -1,31 +1,52 @@
 import re
-from typing import Callable, Dict, List, Union
+from typing import Callable, Dict, Union
 
 import torch
-
 from .comparators import Comparator
-from .formula import Formula
+from .formula import Formula, EvaluationMethod
 
 
 class Constraint:
     '''
-    Given a string representation of a constraint (equality or inequality), 
-    and a set of variables (state, value, prices) in a model, 
-    parse the equation to a pytorch function that can be evaluated. 
+    Given a string representation of a constraint (equality or inequality),
+    and a set of variables (state, value, prices) in a model,
+    parse the equation to a pytorch function that can be evaluated.
 
-    Label is used to identify the corresponding system to use 
+    Label is used to identify the corresponding system to use
     when the constraint is satisfied.
 
-    If the constraint is an inequality, loss should be penalized 
-    whenever the inequality is not satisfied. 
-    e.g. if eq is L1 > L2, it is not satisfied when L2 - L1 >= 0, 
+    If the constraint is an inequality, loss should be penalized
+    whenever the inequality is not satisfied.
+    e.g. if eq is L1 > L2, it is not satisfied when L2 - L1 >= 0,
     so we should formulate the Loss as ReLU(L2-L1).
     '''
-    def __init__(self, lhs, comparator: Comparator, rhs, label, latex_var_mapping: Dict[str, str] = {}):
-        pass 
+
+    def __init__(self, lhs: str, comparator: Comparator, rhs: str, label: str, latex_var_mapping: Dict[str, str] = {}):
+        '''
+        Parse the constraint LHS and RHS separately, and initialize the comparator.
+        '''
+        self.label = label
+        self.lhs = Formula(lhs, EvaluationMethod.Eval, latex_var_mapping)
+        self.rhs = Formula(rhs, EvaluationMethod.Eval, latex_var_mapping)
+        self.comparator = comparator
 
     def eval(self, available_functions: Dict[str, Callable], variables: Dict[str, torch.Tensor]):
         '''
-            compute the loss based on the inequality constraint
+        Compute the loss based on the inequality constraint
         '''
-        pass
+        lhs_eval = self.lhs.eval(available_functions, variables)
+        rhs_eval = self.rhs.eval(available_functions, variables)
+
+        if self.comparator == Comparator.EQ:
+            return torch.mean(torch.square(lhs_eval - rhs_eval))
+        elif self.comparator == Comparator.GT:
+            return torch.mean(torch.relu(rhs_eval - lhs_eval))
+        elif self.comparator == Comparator.LT:
+            return torch.mean(torch.relu(lhs_eval - rhs_eval))
+
+    def __str__(self):
+        str_repr = f"{self.label}: \n"
+        str_repr += f"LHS: {self.lhs.formula_str}\n"
+        str_repr += f"Comparator: {self.comparator}\n"
+        str_repr += f"RHS: {self.rhs.formula_str}"
+        return str_repr
--- deepMacroFin/evaluations/hjb_equation.py
+++ deepMacroFin/evaluations/hjb_equation.py
@@ -3,25 +3,47 @@ from enum import Enum
 from typing import Callable, Dict, List, Union
 
 import torch
-
 from .formula import Formula
+
+
+class EvaluationMethod(str, Enum):
+    Eval = "eval"
+    Sympy = "sympy"
+    AST = "ast"
 
 
 class HJBEquation:
     '''
-    Given a string representation of a Hamilton-Jacobi-Bellman equation, 
-    and a set of variables in a model, 
+    Given a string representation of a Hamilton-Jacobi-Bellman equation,
+    and a set of variables in a model,
     parse the equation to a pytorch function that can be evaluated.
 
     This is used to define the loss function, in the form of a maximization/minimization problem.
 
     The formula classes should be able to parse the equation
     '''
-    def __init__(self, eq, label, latex_var_mapping: Dict[str, str] = {}):
-        pass
+    def __init__(self, eq: str, label: str, latex_var_mapping: Dict[str, str] = {}):
+        '''
+        Parse the equation LHS and RHS of `eq` separately,
+        firstly make sure non-latex version can be parsed correctly.
+        '''
+        assert "=" in eq, f"The equation ({eq}) does not contain '='."
+        self.label = label
+        self.eq = eq.replace("==", "=")  # Ensure single equals for assignment
+        eq_splitted = self.eq.split("=")
+        self.lhs = Formula(eq_splitted[0], EvaluationMethod.Eval, latex_var_mapping)
+        self.rhs = Formula(eq_splitted[1], EvaluationMethod.Eval, latex_var_mapping)
 
     def eval(self, available_functions: Dict[str, Callable], variables: Dict[str, torch.Tensor]):
         '''
-            evaluate the function, compute MSE with 0, return the value
+        Evaluate the function, compute MSE with 0, return the value
         '''
-        pass
+        lhs_eval = self.lhs.eval(available_functions, variables)
+        rhs_eval = self.rhs.eval(available_functions, variables)
+        return torch.mean(torch.square(lhs_eval - rhs_eval))
+
+    def __str__(self):
+        str_repr = f"{self.label}: \n"
+        str_repr += f"Raw input: {self.eq};\n"
+        str_repr += f"Parsed: {self.lhs.formula_str}={self.rhs.formula_str}"
+        return str_repr
